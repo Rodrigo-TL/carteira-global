abc701e1f7f2217d6f101abca44bdf94ef72ddb0
marques-gabriell <magicomnstr@gmail.com>
2026-07-02 00:35:04 +0000

    Ativando a funcionalidade do botão de converter em Dolár.

--- pagina inicial/formulario.js
+++ pagina inicial/formulario.js
@@ -13,3 +13,29 @@ fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL`)
         .catch(function() {
             alert("Erro ao buscar cotação.");
         })
+
+//Buscando a cotação
+let cotacaoDolar;
+    fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL`)
+        .then(function(resposta){
+            return resposta.json();
+        })
+        .then(function(dados){
+            cotacaoDolar = Number(dados.USDBRL.bid);
+            document.getElementById("valor-usd").innerText = cotacaoDolar;
+        })
+
+        .catch(function(){
+            alert("Erro ao buscar cotação");
+        });
+//Criando a funcionalidade do botão de converter o valor em dolar
+function dolar(){
+    let total =
+        document.getElementById("total").value;
+
+    let resultado =
+        Number(total) / cotacaoDolar;
+
+    document.getElementById("resultado").innerText =
+        resultado.toFixed(2);
+}
